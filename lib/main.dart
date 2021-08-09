515f2ffb908ceeab80c2f65d2399d94c52d0c2f9
shashi kiran katkam <85386298+shashikiran918@users.noreply.github.com>
2021-08-09 19:55:55 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:tabbardotindicator/tab_indicator/dot_indicator.dart';
+import 'package:tabbardotindicator/tab_indicator/outline_indicator.dart';
+void main()=>runApp(MaterialApp(home:MainTabBar()));
+
+class MainTabBar extends StatefulWidget {
+  const MainTabBar({Key? key}) : super(key: key);
+
+  @override
+  _MainTabBarState createState() => _MainTabBarState();
+}
+
+class _MainTabBarState extends State<MainTabBar>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  late Decoration _currentIndicator;
+
+  final Map<String, Decoration> _indicators = {
+    'Dot Indicator': DotIndicator(),
+    'Outline Indicator': OutlineIndicator(),
+  };
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 3, vsync: this);
+    _currentIndicator = _indicators.values.first;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar,
+      body: _body,
+    );
+  }
+
+  AppBar get _appBar => AppBar(
+    title: Text('SHASHI'),
+    centerTitle: true,
+    actions: [
+      IconButton(
+        icon: Icon(Icons.filter_list_rounded),
+        onPressed: () => _showBottomSheet(),
+      ),
+    ],
+    bottom: TabBar(
+      controller: _tabController,
+      indicator: _currentIndicator,
+      tabs: [
+        Tab(icon: Icon(Icons.home)),
+        Tab(icon: Icon(Icons.dashboard)),
+        Tab(icon: Icon(Icons.settings)),
+      ],
+    ),
+  );
+
+  Widget get _body => TabBarView(
+    controller: _tabController,
+    children: [
+      Icon(Icons.home, size: 100),
+      Icon(Icons.dashboard, size: 100),
+      Icon(Icons.settings, size: 100),
+    ],
+  );
+
+  void _showBottomSheet() {
+    final List<Widget> tiles = [];
+    _indicators.forEach((key, value) {
+      var tile = ListTile(
+        title: Text(key),
+        onTap: () {
+          setState(() {
+            _currentIndicator = value;
+          });
+          Navigator.pop(context);
+        },
+      );
+      tiles.add(tile);
+    });
+
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: tiles,
+      ),
+    );
+  }
+}
